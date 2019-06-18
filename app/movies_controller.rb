55aba80b49f8697c216c69d3cab9187accb53564
Brian Salemi <briandsalemi@gmail.com>
2019-06-18 14:43:49 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -30,6 +30,7 @@ def can_be_created_in_a_block(args = {})
   # title == "Home Alone"
   # release_date == 1990
   Movie.create do |m|
+    binding.pry
     m.string :title
     m.integer :release_date
     m.string :director
